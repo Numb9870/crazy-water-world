3dbaffc2352efe1d9296fdacde94afa804032ba5
Numb9870 <fgy_game@163.com>
2026-09-18 11:16:11 +0800

    1.0.6 添加氪金vip内容

--- docs/data/CDKEY.ts
+++ docs/data/CDKEY.ts
@@ -1,58 +1,426 @@
 export const CDKEY = [
-    {
-        date: '20260916',
-        key: 'mj14789',
-    },
-    {
-        date: '20260915',
-        key: 'yxzBGPP93X',
-    },
-    {
-        date: '20260915',
-        key: 'SS5J37',
-    },
-    {
-        date: '20260914',
-        key: 'B6C9EO7B',
-    },
-    {
-        date: '20260912',
-        key: 'yxz6JEM282',
-    },
-    {
-        date: '20260912',
-        key: '7SJJ2581',
-    },
-    {
-        date: '20260909',
-        key: 'BEJV1XWU',
-    },
-    {
-        date: '20260908',
-        key: 'yxzBGPP93X',
-    },
-    {
-        date: '20260908',
-        key: '39SJJ14',
-    },
-    {
-        date: '20260907',
-        key: 'SJJ58023',
-    },
-    {
-        date: '20260907',
-        key: '2SJJ5064',
-    },
-    {
-        date: '20260831',
-        key: '73SJJ95',
-    },
-    {
-        date: '20260828',
-        key: '1SJJ9246',
-    },
-    {
-        date: '20260827',
-        key: 'CYKMP7WY',
-    }
-]
+  {
+    date: '20260917',
+    key: 'SSJIE87N',
+  },
+  {
+    date: '20260916',
+    key: 'mj14789',
+  },
+  {
+    date: '20260915',
+    key: 'yxzBGPP93X',
+  },
+  {
+    date: '20260915',
+    key: 'SS5J37',
+  },
+  {
+    date: '20260914',
+    key: 'B6C9EO7B',
+  },
+  {
+    date: '20260912',
+    key: 'yxz6JEM282',
+  },
+  {
+    date: '20260912',
+    key: '7SJJ2581',
+  },
+  {
+    date: '20260909',
+    key: 'BEJV1XWU',
+  },
+  {
+    date: '20260908',
+    key: 'yxzBGPP93X',
+  },
+  {
+    date: '20260908',
+    key: '39SJJ14',
+  },
+  {
+    date: '20260907',
+    key: 'SJJ58023',
+  },
+  {
+    date: '20260907',
+    key: '2SJJ5064',
+  },
+  {
+    date: '20260831',
+    key: '73SJJ95',
+  },
+  {
+    date: '20260828',
+    key: '1SJJ9246',
+  },
+  {
+    date: '20260827',
+    key: 'CYKMP7WY',
+  },
+  {
+    date: '20260825',
+    key: 'yxzP9PKF4E',
+  },
+  {
+    date: '20260824',
+    key: 'xyxz30w666',
+  },
+  {
+    date: '20260824',
+    key: 'APBG6O46',
+  },
+  {
+    date: '20260824',
+    key: 'SJJ47218',
+  },
+  {
+    date: '20260821',
+    key: '64SJJ72',
+  },
+  {
+    date: '20260820',
+    key: '9MPZQHNQ',
+  },
+  {
+    date: '20260820',
+    key: 'yXz29TVQW2',
+  },
+  {
+    date: '20260819',
+    key: 'SJJ35190',
+  },
+  {
+    date: '20260818',
+    key: 'SJJ10628',
+  },
+  {
+    date: '20260817',
+    key: '0SJJ7429',
+  },
+  {
+    date: '20260816',
+    key: '8SJJ4309',
+  },
+  {
+    date: '20260815',
+    key: 'UGWSXJG6',
+  },
+  {
+    date: '20260815',
+    key: 'X2U0Y1GE',
+  },
+  {
+    date: '20260814',
+    key: '27SJJ51',
+  },
+  {
+    date: '20260813',
+    key: '91SJJ37',
+  },
+  {
+    date: '20260813',
+    key: 'BZNQKX',
+  },
+  {
+    date: '20260812',
+    key: '5SJJ084',
+  },
+  {
+    date: '20260811',
+    key: 'SSJ88999',
+  },
+  {
+    date: '20260810',
+    key: '18SJJ53',
+  },
+  {
+    date: '20260809',
+    key: 'xc999',
+  },
+  {
+    date: '20260809',
+    key: 'xc888',
+  },
+  {
+    date: '20260809',
+    key: 'xc666',
+  },
+  {
+    date: '20260809',
+    key: '小春666',
+  },
+  {
+    date: '20260809',
+    key: '小春999',
+  },
+  {
+    date: '20260809',
+    key: '小春888',
+  },
+  {
+    date: '20260804',
+    key: '5JDWTSDJ',
+  },
+  {
+    date: '20260728',
+    key: 'VXY2CKO2',
+  },
+  {
+    date: '20260724',
+    key: 'xyxz20w666',
+  },
+  {
+    date: '20260723',
+    key: '2IQCXVVC',
+  },
+  {
+    date: '20260720',
+    key: 'ZBZSLBM55',
+  },
+  {
+    date: '20260720',
+    key: 'ZBJFL777',
+  },
+  {
+    date: '20260720',
+    key: 'JSCJQ111',
+  },
+  {
+    date: '20260720',
+    key: '10WDHM',
+  },
+  {
+    date: '20260717',
+    key: 'ckjq065',
+  },
+  {
+    date: '20260717',
+    key: 'zbdlb222',
+  },
+  {
+    date: '20260713',
+    key: 'Timo888',
+  },
+  {
+    date: '20260713',
+    key: 'Timo777',
+  },
+  {
+    date: '20260713',
+    key: 'Timo666',
+  },
+  {
+    date: '20260615',
+    key: 'DYFKSSJ',
+  },
+  {
+    date: '20260610',
+    key: 'ZHIBO777',
+  },
+  {
+    date: '20260530',
+    key: '益世界关注礼',
+  },
+  {
+    date: '20260510',
+    key: 'kt23456',
+  },
+  {
+    date: '20260508',
+    key: '20W9999',
+  },
+  {
+    date: '20260508',
+    key: 'zbjxz55',
+  },
+  {
+    date: '20260508',
+    key: 'CJNLB77',
+  },
+  {
+    date: '20260508',
+    key: 'XQZBDW11',
+  },
+  {
+    date: '20260508',
+    key: 'ZHIBO888',
+  },
+  {
+    date: '20260508',
+    key: 'ZHIBO666',
+  },
+  {
+    date: '20260505',
+    key: 'SW45612',
+  },
+  {
+    date: '20260505',
+    key: 'MJ14789',
+  },
+  {
+    date: '20260430',
+    key: 'SJ78945',
+  },
+  {
+    date: '20260420',
+    key: '10wheli',
+  },
+  {
+    date: '20260420',
+    key: 'FK12345',
+  },
+  {
+    date: '20260409',
+    key: 'SVIP666',
+  },
+  {
+    date: '20260401',
+    key: '水世界琥珀玩家礼',
+  },
+  {
+    date: '20260205',
+    key: 'FKSSJ888',
+  },
+  {
+    date: '20260205',
+    key: 'FKSSJ777',
+  },
+  {
+    date: '20260205',
+    key: 'FKSSJ666',
+  },
+  {
+    date: '20260205',
+    key: 'MIYALOVEU',
+  },
+  {
+    date: '20260205',
+    key: '20wheli',
+  },
+  {
+    date: '20260205',
+    key: 'VIP888',
+  },
+  {
+    date: '20260205',
+    key: 'VIP777',
+  },
+  {
+    date: '20260205',
+    key: 'VIP666',
+  },
+  {
+    date: '20260205',
+    key: '水世界上线礼',
+  },
+  {
+    date: '20260205',
+    key: 'XO1WM6',
+  },
+  {
+    date: '20260205',
+    key: 'SSJR8P',
+  },
+  {
+    date: '20260205',
+    key: 'SSJ1A',
+  },
+  {
+    date: '20260205',
+    key: 'BSSJ4F',
+  },
+  {
+    date: '20260205',
+    key: '9SSJ2N',
+  },
+  {
+    date: '20260205',
+    key: '2SSJ8C',
+  },
+  {
+    date: '20260205',
+    key: 'LSSJ0M',
+  },
+  {
+    date: '20260205',
+    key: 'SSJ3K7',
+  },
+  {
+    date: '20260205',
+    key: 'X5SSJ9',
+  },
+  {
+    date: '20260205',
+    key: '6160P4',
+  },
+  {
+    date: '20260205',
+    key: 'O7L5J5',
+  },
+  {
+    date: '20260205',
+    key: 'N1O40V',
+  },
+  {
+    date: '20260205',
+    key: '5SSJ9L',
+  },
+  {
+    date: '20260205',
+    key: 'FSSJ1M',
+  },
+  {
+    date: '20260205',
+    key: 'USSJ8W',
+  },
+  {
+    date: '20260205',
+    key: 'SSJ6G4',
+  },
+  {
+    date: '20260205',
+    key: '7SSJF2',
+  },
+  {
+    date: '20260205',
+    key: 'SSJ3H5',
+  },
+  {
+    date: '20260205',
+    key: 'SSJ0H8',
+  },
+  {
+    date: '20260205',
+    key: '3JK0',
+  },
+  {
+    date: '20260205',
+    key: 'SSJ7V1',
+  },
+  {
+    date: '20260205',
+    key: 'SSJ2YW',
+  },
+  {
+    date: '20260205',
+    key: '6SSJD9',
+  },
+  {
+    date: '20260205',
+    key: 'PSSJ5Q',
+  },
+  {
+    date: '20260205',
+    key: 'M8SSJ3',
+  },
+  {
+    date: '20260205',
+    key: 'CSSJ5Z',
+  },
+  {
+    date: '20260205',
+    key: '4TSSJ6',
+  },
+];
